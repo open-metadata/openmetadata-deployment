74ebf8d20a4b6e6cd1bc0cd4e3663e0dcfd15534
Andrés Muñoz Vera <andres@getcollate.io>
2025-07-28 15:25:59 +0200

    chore - pin Helm provider version (#50)
    * chore - pin Helm provider version to the latest of v2
    
    * chore - bump providers, Helm to v3 and AWS to v6
    
    * chore - updating readme

--- terraform/hybrid-ingestion-runner/aws/versions.tf
+++ terraform/hybrid-ingestion-runner/aws/versions.tf
@@ -3,7 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
     }
   }
 }
