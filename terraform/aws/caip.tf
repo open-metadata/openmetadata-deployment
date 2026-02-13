c5648ee92f7709861bd14adb0d71890918fed2d8
Andrés Muñoz Vera <andres@getcollate.io>
2026-02-13 12:41:21 +0000

    Assign the new service account to the server & caip (#120)

--- terraform/aws/caip.tf
+++ terraform/aws/caip.tf
@@ -3,6 +3,9 @@ locals {
     "region"                   = var.region
     "collate.hostAndPort"      = "http://openmetadata:8585"
     "imagePullSecrets[0].name" = "omd-registry-credentials"
+    "serviceAccount.name"      = local.omd.server_sa_name
+    "serviceAccount.create"    = "false"
+
   }
 }
 
@@ -11,21 +14,13 @@ resource "helm_release" "caip" {
   name       = "caip"
   repository = "https://open-metadata.github.io/collate-ai-proxy-helm-chart"
   chart      = "collate-ai-proxy"
-  version    = "0.0.1"
-  #version    = local.omd.helm_chart_version
-  namespace = kubernetes_namespace.app.id
-  wait      = false
+  version    = local.omd.helm_chart_version
+  namespace  = kubernetes_namespace.app.id
+  wait       = false
   set = [for key, value in merge(local.caip, var.caip_helm_values) :
     {
       "name"  = key
       "value" = value
     }
   ]
-
-  #  set_list = [
-  #    {
-  #      name  = "imagePullSecrets"
-  #      value = [{ "name": "omd-registry-credentials"}]
-  #    }
-  #  ]
 }
