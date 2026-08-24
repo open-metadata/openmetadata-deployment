f76f86e8738db577c063b0f3afc1358b922610bf
Andrés Muñoz <andres@getcollate.io>
2026-08-24 17:04:38 +0200

    CHORE: release 2.0.0

--- terraform/hybrid-ingestion-runner/aws/variables.tf
+++ terraform/hybrid-ingestion-runner/aws/variables.tf
@@ -14,7 +14,7 @@ variable "region" {
 variable "release_version" {
   type        = string
   description = "The Hybrid Ingestion Runner version to deploy."
-  default     = "1.12.14"
+  default     = "2.0.0"
 }
 
 variable "namespace" {
